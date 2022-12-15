aecd139ca24c319265180f372f3cd7b107a0932f
Pete-Fowler <fowler.pete@gmail.com>
2022-12-15 10:22:05 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Program.cs
+// The Main() method
+
+IEnumerable<string> Suits()
+{
+    yield return "clubs";
+    yield return "diamonds";
+    yield return "hearts";
+    yield return "spades";
+}
+
+IEnumerable<string> Ranks()
+{
+    yield return "two";
+    yield return "three";
+    yield return "four";
+    yield return "five";
+    yield return "six";
+    yield return "seven";
+    yield return "eight";
+    yield return "nine";
+    yield return "ten";
+    yield return "jack";
+    yield return "queen";
+    yield return "king";
+    yield return "ace";
+}
+
+var startingDeck = from s in Suits()
+                   from r in Ranks()
+                   select new { Suit = s, Rank = r };
+
+foreach (var card in startingDeck)
+{
+    Console.WriteLine(card);
+}
+
+Console.WriteLine(startingDeck.Count());
